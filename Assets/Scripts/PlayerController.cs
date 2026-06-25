cf1aa4bdf30287fe2be383fa9cd72ec425580c44
Lappland <B4d4ssG04t@gmail.com>
2026-06-25 07:00:08 -0300

    Creation of TestDummy For enemy
    Added a simple enemy to test the damage EnemyCombat and PlayerHealth Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,6 +30,7 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        _canMove = true;
         _rb = GetComponent<Rigidbody2D>();
         if (_rb == null)
         {
