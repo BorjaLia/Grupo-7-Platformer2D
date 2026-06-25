3f14806d4fad6fb9b798c126f1581fe2ff798ec2
Lappland <B4d4ssG04t@gmail.com>
2026-06-25 01:24:53 -0300

    Addition of Player Illumination direction swap
    Added the direction swap for the sprite light of the character so it changes its facing direction  with the players facing direction.
    If there is not light linked to the code then does nothing.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,21 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement Settings")]
+    [Header("Player Settings")]
+    [SerializeField] private float lives = 5.0f;
+    [SerializeField] private float playerAttackForce = 10.0f;
+    
+    [Header("Player Movement Settings")]
     [SerializeField] private float playerSpeed = 5.0f;
     [SerializeField] private float playerJumpForce = 15.0f;
     [SerializeField] private float playerDodgeForce = 15.0f;
     [SerializeField] private float playerAirControlMultiplier = 0.5f;
     
-    [Header("Animator Settings")]
+    [Header("Player Animator")]
     [SerializeField] private Animator animator;
+    
+    [Header("References")]
+    [SerializeField] private Transform lightPivot; 
     
     private Rigidbody2D _rb;
     private SpriteRenderer _spriteRenderer;
@@ -36,17 +43,14 @@ public class PlayerController : MonoBehaviour
     {
         _horizontalInput = Input.GetAxis("Horizontal");
         
-        if (_horizontalInput > 0.01f)
-            _spriteRenderer.flipX = false;
-        else if (_horizontalInput < -0.01f)
-            _spriteRenderer.flipX = true;
-        
         if (Input.GetButtonDown("Jump") && _isGrounded)
         {
             _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, playerJumpForce);
         }
         
         animator.SetFloat("Speed", Mathf.Abs(_rb.linearVelocity.x));
+        
+        UpdateFacingDirection(); 
     }
     
     private void FixedUpdate()
@@ -56,14 +60,30 @@ public class PlayerController : MonoBehaviour
         _rb.linearVelocity = targetVelocity;
     }
 
+    
+    private void UpdateFacingDirection() // Function That is responsible for switching the sprite and if the character has one the illumination of it 
+    {
+        if (_horizontalInput > 0.01f)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (_horizontalInput < -0.01f)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        
+        if (lightPivot == null) return;
+        var facingDirection = _spriteRenderer.flipX ? -1f : 1f;
+        lightPivot.localScale = new Vector3(facingDirection, 1f, 1f);
+    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground")) 
         { 
             _isGrounded = true;
         }
-    }
-    
+    } 
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
