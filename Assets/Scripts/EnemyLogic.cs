b29b9e9704e4352e4fc7e627613b14fb46e42c70
Lappland <B4d4ssG04t@gmail.com>
2026-06-28 13:04:04 -0300

    EnemyLogic update
    Now the enemy chases the player in a simple way and flips side depending on what side the player is

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -1,22 +1,35 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class EnemyLogic : MonoBehaviour
 {
+    [SerializeField] private EnemyData enemyData;
+    
+    private Transform _target;
+    private Rigidbody2D _rb;
     private SpriteRenderer _spriteRenderer;
     private Light2D _enemyLight;
     private Transform _lightPivot; 
+    private EnemyData _runTimeEnemyData;
     
-    private float _horizontalInput;
+    private float _facingDirection = -1;
+    private bool _isChasing = false;
     
     private void Awake()
     {
+        _rb = GetComponent<Rigidbody2D>();
+        
+        if (enemyData != null)
+            _runTimeEnemyData = Instantiate(enemyData);
+        
         _spriteRenderer = GetComponent<SpriteRenderer>();
         if (_spriteRenderer == null)
         { 
             Debug.LogWarning("No SpriteRenderer found on the player.");
         }
+        
         _enemyLight = GetComponentInChildren<Light2D>();
         if (_enemyLight == null)
         {
@@ -25,9 +38,24 @@ public class EnemyLogic : MonoBehaviour
         _lightPivot = _enemyLight.transform;
     }
 
+    private void Start()
+    {
+        
+    }
+
     private void Update()
     {
-        UpdateFacingDirection();
+        if (_isChasing == true)
+        {
+            if (_target.position.x > transform.position.x && _facingDirection == -1 ||
+                _target.position.x < transform.position.x && _facingDirection == 1)
+            {
+                UpdateFacingDirection();
+            }
+            
+            Vector2 direction = (_target.position - transform.position).normalized;
+            _rb.linearVelocity = direction * _runTimeEnemyData.speed;
+        }
     }
 
     public void DmgBounce(Vector2 direction)
@@ -37,17 +65,32 @@ public class EnemyLogic : MonoBehaviour
     
     private void UpdateFacingDirection() // Function That is responsible for switching the sprite and if the character has a light sprite, switch it too 
     {
-        if (_horizontalInput > 0.01f)
-        {
-            _spriteRenderer.flipX = false;
-        }
-        else if (_horizontalInput < -0.01f)
-        {
-            _spriteRenderer.flipX = true;
-        }
+        _facingDirection *= -1;
         
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
         if (_lightPivot == null) return;
         var facingDirection = _spriteRenderer.flipX ? -1f : 1f;
         _lightPivot.localScale = new Vector3(facingDirection, 1f, 1f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            if (_target == null)
+            {
+                _target = collision.transform;
+            }
+            _isChasing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            _rb.linearVelocity = Vector2.zero;
+            _isChasing = false;
+        }
     }
 }
