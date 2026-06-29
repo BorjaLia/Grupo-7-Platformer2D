c74bfce34bac3b49166fbad5f3bbf4b56c5a9639
BorjaLia <borja.lia.deichler@gmail.com>
2026-06-29 12:01:24 -0300

    Fix settings and audio manager conection

--- Assets/Scripts/Menu/SettingsManager.cs
+++ Assets/Scripts/Menu/SettingsManager.cs
@@ -9,55 +9,30 @@ public class SettingsManager : MonoBehaviour
     [SerializeField] private Slider sfxSlider;
     [SerializeField] private Slider uiSlider;
 
-    //private void Start()
-    //{
-    //    if (DataManager.s_instance != null)
-    //    {
-    //        masterSlider.value = DataManager.s_instance.masterVolume;
-    //        musicSlider.value = DataManager.s_instance.musicVolume;
-    //        sfxSlider.value = DataManager.s_instance.sfxVolume;
-    //        uiSlider.value = DataManager.s_instance.uiVolume;
+    private void Start()
+    {
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        uiSlider.onValueChanged.AddListener(SetUIVolume);
+    }
 
-    //        AudioManager.s_instance.SetVolume("MasterVolume", masterSlider.value);
-    //        AudioManager.s_instance.SetVolume("MusicVolume", musicSlider.value);
-    //        AudioManager.s_instance.SetVolume("SFXVolume", sfxSlider.value);
-    //        AudioManager.s_instance.SetVolume("UIVolume", uiSlider.value);
-    //    }
+    private void SetMasterVolume(float value)
+    {
+        SoundManager.SetSoundVolume("MasterVolume", value);
+    }
 
-    //    masterSlider.onValueChanged.AddListener(SetMasterVolume);
-    //    musicSlider.onValueChanged.AddListener(SetMusicVolume);
-    //    sfxSlider.onValueChanged.AddListener(SetSFXVolume);
-    //    uiSlider.onValueChanged.AddListener(SetUIVolume);
-    //}
+    private void SetMusicVolume(float value)
+    {
+        SoundManager.SetSoundVolume("MusicVolume", value);
+    }
 
-    //private void SetMasterVolume(float value)
-    //{
-    //    AudioManager.s_instance.SetVolume("MasterVolume", value);
-    //    SaveAllVolumes();
-    //}
-
-    //private void SetMusicVolume(float value)
-    //{
-    //    AudioManager.s_instance.SetVolume("MusicVolume", value);
-    //    SaveAllVolumes();
-    //}
-
-    //private void SetSFXVolume(float value)
-    //{
-    //    AudioManager.s_instance.SetVolume("SFXVolume", value);
-    //    SaveAllVolumes();
-    //}
-    //private void SetUIVolume(float value)
-    //{
-    //    AudioManager.s_instance.SetVolume("UIVolume", value);
-    //    SaveAllVolumes();
-    //}
-
-    //private void SaveAllVolumes()
-    //{
-    //    if (DataManager.s_instance != null)
-    //    {
-    //        DataManager.s_instance.SaveVolumes(masterSlider.value, musicSlider.value, sfxSlider.value,uiSlider.value);
-    //    }
-    //}
+    private void SetSFXVolume(float value)
+    {
+        SoundManager.SetSoundVolume("SFXVolume", value);
+    }
+    private void SetUIVolume(float value)
+    {
+        SoundManager.SetSoundVolume("UIVolume", value);
+    }
 }
